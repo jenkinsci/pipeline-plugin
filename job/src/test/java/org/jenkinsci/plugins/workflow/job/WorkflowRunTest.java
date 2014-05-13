e3b022aebfa04c9c5015e95159e4c0fa2bc9fec8
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-13 15:45:29 -0700

    'dsl' is no more

--- job/src/test/java/org/jenkinsci/plugins/workflow/job/WorkflowRunTest.java
+++ job/src/test/java/org/jenkinsci/plugins/workflow/job/WorkflowRunTest.java
@@ -70,7 +70,7 @@ public class WorkflowRunTest {
     }
 
     @Test public void parameters() throws Exception {
-        p.setDefinition(new CpsFlowDefinition("dsl.node {dsl.sh('echo param=' + PARAM)}"));
+        p.setDefinition(new CpsFlowDefinition("with.node {sh('echo param=' + PARAM)}"));
         p.addProperty(new ParametersDefinitionProperty(new StringParameterDefinition("PARAM", null)));
         WorkflowRun b = r.assertBuildStatusSuccess(p.scheduleBuild2(0, new ParametersAction(new StringParameterValue("PARAM", "value"))));
         r.assertLogContains("param=value", b);
