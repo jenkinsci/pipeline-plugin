031c22cca2fc21b982fa0eaf7319f79c4ae023a7
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-13 15:47:25 -0700

    groovy expects array

--- cps/src/main/java/org/jenkinsci/plugins/workflow/cps/CpsBuiltinSteps.java
+++ cps/src/main/java/org/jenkinsci/plugins/workflow/cps/CpsBuiltinSteps.java
@@ -2,10 +2,6 @@ package org.jenkinsci.plugins.workflow.cps;
 
 import groovy.lang.Closure;
 
-import java.util.Arrays;
-
-import static java.util.Arrays.asList;
-
 /**
  * "Built-in" functions for CPS groovy script.
  *
@@ -18,14 +14,18 @@ public class CpsBuiltinSteps {
      * Executes a shell script in the current workspace.
      */
     public static int sh(Object... args) {
-        return (Integer)dsl().invokeMethod("sh", asList(args));
+        return (Integer)dsl().invokeMethod("sh", asArray(args));
     }
 
     public static void retry(int n, Closure body) {
-        dsl().invokeMethod("retry", asList(n, body));
+        dsl().invokeMethod("retry", asArray(n, body));
     }
 
     private static DSL dsl() {
         return new DSL(CpsThread.current().group.getExecution().getOwner());
+    }
+
+    private static Object[] asArray(Object... args) {
+        return args;
     }
 }
