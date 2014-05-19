d28c007652b05aba8ead8529973806d30c515b28
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-19 10:25:35 -0700

    Adding a new action to signify the invocation of a body

---
+++ flow-api/src/main/java/org/jenkinsci/plugins/workflow/actions/BodyInvocationAction.java
@@ -0,0 +1,35 @@
+package org.jenkinsci.plugins.workflow.actions;
+
+import com.google.common.util.concurrent.FutureCallback;
+import hudson.model.InvisibleAction;
+import org.jenkinsci.plugins.workflow.graph.BlockStartNode;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+
+/**
+ * Attached to {@link BlockStartNode} to indicate that this block
+ * represents {@linkplain StepContext#invokeBodyLater(FutureCallback, Object...) an invocation of body block}.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class BodyInvocationAction extends InvisibleAction implements FlowNodeAction {
+    private final String parentId;
+
+    /**
+     * @param stepBlock
+     *      Reference to the block that signifies the enclosing block (which corresponds
+     *      to the invocation of the step itself.)
+     */
+    public BodyInvocationAction(BlockStartNode stepBlock) {
+        this.parentId = stepBlock.getId();
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    @Override
+    public void onLoad(FlowNode parent) {
+        // noop
+    }
+}
