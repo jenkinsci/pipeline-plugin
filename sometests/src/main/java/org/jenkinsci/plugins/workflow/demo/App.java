ee15dbdd89228f51479f4998028bd5aa727e2160
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-06-12 19:14:34 -0700

    Implemented the first demo app and its integration test

---
+++ sometests/src/main/java/org/jenkinsci/plugins/workflow/demo/App.java
@@ -0,0 +1,7 @@
+package org.jenkinsci.plugins.workflow.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ sometests/src/test/java/org/jenkinsci/plugins/workflow/demo/AppTest.java
@@ -0,0 +1,25 @@
+package org.jenkinsci.plugins.workflow.demo;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URL;
+
+/**
+ * Integration test for webapp.
+ */
+public class AppTest extends Assert {
+    @Test
+    public void testApp() throws Exception {
+        URL app = getSUT();
+        String contents = IOUtils.toString(app.openStream());
+        assertTrue(contents.contains("Hello Jenkins!"));
+    }
+
+    private URL getSUT() throws Exception {
+        String url = System.getProperty("url");
+        assertTrue("Subject under test should be passed in via -Durl=...", url!=null);
+        return new URL(url);
+    }
+}
