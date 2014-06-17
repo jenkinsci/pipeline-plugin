587b4a45653a1c11046ab1a2807a0e44a4cc0eff
Jesse Glick <jglick@cloudbees.com>
2014-06-17 10:43:21 -0400

    Move functions to end of script for better appearance.

--- demo/cd.groovy
+++ demo/cd.groovy
@@ -1,24 +1,6 @@
 // TODO consider using https://github.com/cloudbees/jenkins-docker-executors to host everything (install graphviz on Jenkins node)
 // Prep: mkdir /tmp/webapps && docker run -p 80:8080 -v /tmp/webapps:/opt/jetty/webapps jglick/jetty-demo &
 
-def deploy(war, id) {
-    sh("cp ${war} /tmp/webapps/${id}.war")
-}
-
-def undeploy(id) {
-    sh("rm /tmp/webapps/${id}.war")
-}
-
-def runWithServer(body) {
-    def id = UUID.randomUUID().toString()
-    deploy('target/x.war', id)
-    try {
-        body.call("http://localhost/${id}/");
-    } finally {
-        undeploy(id)
-    }
-}
-
 steps.segment('Dev')
 with.node('master') {
     def src = 'https://github.com/jenkinsci/workflow-plugin-pipeline-demo.git'
@@ -46,4 +28,22 @@ with.node('master') {
     steps.unarchive(mapping: ['target/x.war' : 'x.war'])
     deploy('x.war', 'production')
     steps.echo 'Deployed to http://localhost/production/'
+}
+
+def deploy(war, id) {
+    sh("cp ${war} /tmp/webapps/${id}.war")
+}
+
+def undeploy(id) {
+    sh("rm /tmp/webapps/${id}.war")
+}
+
+def runWithServer(body) {
+    def id = UUID.randomUUID().toString()
+    deploy('target/x.war', id)
+    try {
+        body.call("http://localhost/${id}/");
+    } finally {
+        undeploy(id)
+    }
 }
