fb47319948e25142a9ba925afc3e85766155f85a
Jesse Glick <jglick@cloudbees.com>
2014-06-17 11:18:57 -0400

    Provisionally changing SegmentStep to use the term "stage" since this seemed more familiar to people. Leaving the internal class name and so on the same for now.

--- demo/cd.groovy
+++ demo/cd.groovy
@@ -1,13 +1,13 @@
 // TODO consider using https://github.com/cloudbees/jenkins-docker-executors to host everything (install graphviz on Jenkins node)
 // Prep: mkdir /tmp/webapps && docker run -p 80:8080 -v /tmp/webapps:/opt/jetty/webapps jglick/jetty-demo &
 
-steps.segment('Dev')
+steps.stage('Dev')
 with.node('master') {
     def src = 'https://github.com/jenkinsci/workflow-plugin-pipeline-demo.git'
     steps.git(url: src)
     sh('mvn clean package')
     steps.archive('target/x.war')
-    segment('QA')
+    stage('QA')
     parallel(sometests: {
         runWithServer {url ->
             sh("mvn -f sometests/pom.xml test -Durl=${url}")
@@ -17,12 +17,12 @@ with.node('master') {
             sh("mvn -f sometests/pom.xml test -Durl=${url}") // TODO add other test module
         }
     })
-    steps.segment(value: 'Staging', concurrency: 1)
+    steps.stage(value: 'Staging', concurrency: 1)
     deploy('target/x.war', 'staging')
 }
 steps.input(message: "Does http://localhost/staging/ look good?")
 steps.checkpoint('Before production')
-steps.segment(value: 'Production', concurrency: 1)
+steps.stage(value: 'Production', concurrency: 1)
 with.node('master') {
     sh('curl -I http://localhost/staging/')
     steps.unarchive(mapping: ['target/x.war' : 'x.war'])
