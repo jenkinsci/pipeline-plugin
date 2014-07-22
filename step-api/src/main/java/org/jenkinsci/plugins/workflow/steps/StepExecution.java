71d7971ca05b5171bedf8384d51642eef39f413d
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-07-22 14:09:46 +0200

    Move to StepExecution to make discovery easier

--- step-api/src/main/java/org/jenkinsci/plugins/workflow/steps/StepExecution.java
+++ step-api/src/main/java/org/jenkinsci/plugins/workflow/steps/StepExecution.java
@@ -1,14 +1,18 @@
 package org.jenkinsci.plugins.workflow.steps;
 
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
- *
+ * Scoped to a single execution of {@link Step}, and provides insights into what's going on
+ * asynchronously and aborting the activity if need be.
  *
  * @author Kohsuke Kawaguchi
  * @author Jesse Glick
@@ -64,4 +68,32 @@ public abstract class StepExecution {
      * whether or not it was asked to stop.
      */
     public void stop() {}
+
+    /**
+     * Apply the given function to all the active running {@link StepExecution}s in the system.
+     *
+     * @return
+     *      Future object that signals when the function application is complete.
+     * @see StepExecutionIterator
+     */
+    public static ListenableFuture<?> applyAll(Function<StepExecution,Void> f) {
+        List<ListenableFuture<?>> futures = new ArrayList<ListenableFuture<?>>();
+        for (StepExecutionIterator i : StepExecutionIterator.all())
+            futures.add(i.apply(f));
+        return Futures.allAsList(futures);
+    }
+
+    /**
+     * Applies only to the specific subtypes.
+     */
+    public static <T extends StepExecution> ListenableFuture<?> applyAll(final Class<T> type, final Function<T,Void> f) {
+        return applyAll(new Function<StepExecution, Void>() {
+            @Override
+            public Void apply(StepExecution e) {
+                if (type.isInstance(e))
+                    f.apply(type.cast(e));
+                return null;
+            }
+        });
+    }
 }
--- step-api/src/main/java/org/jenkinsci/plugins/workflow/steps/StepExecutionIterator.java
+++ step-api/src/main/java/org/jenkinsci/plugins/workflow/steps/StepExecutionIterator.java
@@ -1,14 +1,10 @@
 package org.jenkinsci.plugins.workflow.steps;
 
 import com.google.common.base.Function;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 import jenkins.model.Jenkins;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Enumerates active running {@link StepExecution}s in the system.
@@ -29,26 +25,5 @@ public abstract class StepExecutionIterator implements ExtensionPoint {
 
     public static ExtensionList<StepExecutionIterator> all() {
         return Jenkins.getInstance().getExtensionList(StepExecutionIterator.class);
-    }
-
-    public static ListenableFuture<?> applyAll(Function<StepExecution,Void> f) {
-        List<ListenableFuture<?>> futures = new ArrayList<ListenableFuture<?>>();
-        for (StepExecutionIterator i : all())
-            futures.add(i.apply(f));
-        return Futures.allAsList(futures);
-    }
-
-    /**
-     * Applies only to the specific subtypes.
-     */
-    public static final <T extends StepExecution> ListenableFuture<?> applyAll(final Class<T> type, final Function<T,Void> f) {
-        return applyAll(new Function<StepExecution, Void>() {
-            @Override
-            public Void apply(StepExecution e) {
-                if (type.isInstance(e))
-                    f.apply(type.cast(e));
-                return null;
-            }
-        });
     }
 }
