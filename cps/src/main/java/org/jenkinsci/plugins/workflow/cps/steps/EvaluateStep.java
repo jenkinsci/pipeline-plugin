55a75e4eece0e0f77c677509e2f2804012e7dafe
Jesse Glick <jglick@cloudbees.com>
2014-10-06 11:56:23 -0400

    Including EvaluateStepExecution in the same compilation unit for readability. Also adding a couple comments about things that might not yet work.

--- cps/src/main/java/org/jenkinsci/plugins/workflow/cps/steps/EvaluateStep.java
+++ cps/src/main/java/org/jenkinsci/plugins/workflow/cps/steps/EvaluateStep.java
@@ -1,8 +1,14 @@
 package org.jenkinsci.plugins.workflow.cps.steps;
 
+import com.google.inject.Inject;
+import groovy.lang.GroovyShell;
 import hudson.Extension;
+import hudson.FilePath;
+import org.jenkinsci.plugins.workflow.cps.CpsThread;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -14,7 +20,7 @@ public class EvaluateStep extends AbstractStepImpl {
     /**
      * Relative path of the script within the current workspace.
      */
-    /*package*/ final String path;
+    private final String path;
 
     @DataBoundConstructor
     public EvaluateStep(String value) {
@@ -37,4 +43,30 @@ public class EvaluateStep extends AbstractStepImpl {
             return "Evaluate a Groovy source file into the workflow script";
         }
     }
+
+    public static class EvaluateStepExecution extends StepExecution {
+        @StepContextParameter
+        private transient FilePath cwd;
+
+        @Inject
+        private EvaluateStep step;
+
+        @Override
+        public boolean start() throws Exception {
+            GroovyShell shell = CpsThread.current().getExecution().getShell();
+
+            // this might throw CpsCallableInvocation to trigger async execution
+            // TODO in that case what happens to the return value?
+            Object o = shell.evaluate(cwd.child(step.path).readToString());
+
+            getContext().onSuccess(o);
+            return true;
+        }
+
+        @Override
+        public void stop() throws Exception {
+            // TODO is there a test confirming that this gets passed on to the running step inside the evaluated script?
+        }
+    }
+
 }
