65d668a577692f59588209e280202a4fc52bbb9e
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-10-14 14:49:41 -0700

    For handling pause inside the loaded script correctly, the script should execute as the body block

--- cps/src/main/java/org/jenkinsci/plugins/workflow/cps/steps/LoadStepExecution.java
+++ cps/src/main/java/org/jenkinsci/plugins/workflow/cps/steps/LoadStepExecution.java
@@ -1,36 +1,64 @@
 package org.jenkinsci.plugins.workflow.cps.steps;
 
 import com.google.inject.Inject;
+import groovy.lang.Closure;
 import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 import hudson.FilePath;
+import hudson.model.Action;
+import org.jenkinsci.plugins.workflow.cps.CpsStepContext;
 import org.jenkinsci.plugins.workflow.cps.CpsThread;
 import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
 import org.jenkinsci.plugins.workflow.steps.StepExecution;
 
+import java.util.Collections;
+
 /**
-* @author Kohsuke Kawaguchi
-*/
+ * Loads another Groovy script file and executes it.
+ *
+ * @author Kohsuke Kawaguchi
+ */
 public class LoadStepExecution extends StepExecution {
     @StepContextParameter
     private transient FilePath cwd;
 
     @Inject
-    private LoadStep step;
+    private transient LoadStep step;
 
     @Override
     public boolean start() throws Exception {
+        CpsStepContext cps = (CpsStepContext) getContext();
+        CpsThread t = CpsThread.current();
+
         GroovyShell shell = CpsThread.current().getExecution().getShell();
 
-        // this might throw CpsCallableInvocation to trigger async execution
-        // TODO in that case what happens to the return value?
-        Object o = shell.evaluate(cwd.child(step.getPath()).readToString());
+        Script script = shell.parse(cwd.child(step.getPath()).readToString());
 
-        getContext().onSuccess(o);
-        return true;
+        // execute body as another thread that shares the same head as this thread
+        // as the body can pause.
+        cps.invokeBodyLater(
+                t.getGroup().export(asCallable(script)),
+                cps, // when the body is done, the load step is done
+                Collections.<Action>emptyList()
+        );
+
+        return false;
+    }
+
+    // TODO: this should be another overload of the export method
+    private Closure asCallable(final Script script) {
+        return new Closure(null) {
+            @Override
+            public Object call() {
+                return script.run();
+            }
+        };
     }
 
     @Override
     public void stop() throws Exception {
-        // TODO is there a test confirming that this gets passed on to the running step inside the evaluated script?
+        // noop
+        //
+        // the head of the CPS thread that's executing the body should stop and that's all we need to do.
     }
 }
