6fd47a3bfa55663c42db14ac6f4c86cf31b5317f
Jesse Glick <jglick@cloudbees.com>
2014-11-18 15:48:58 -0500

    [JENKINS-25504] Trying to at least remove the trigger for the exceptions encountered when shutting down Jenkins. Does not address the general possibility of the bogus NotSerializableException on Executor. Does not have anything to do with actual interruption of an executor step (as opposed to the InterruptedException thrown during shutdown).

--- support/src/main/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepExecution.java
+++ support/src/main/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepExecution.java
@@ -43,7 +43,6 @@ import org.jenkinsci.plugins.durabletask.executors.ContinuedTask;
 import org.jenkinsci.plugins.workflow.flow.FlowExecution;
 import org.jenkinsci.plugins.workflow.graph.FlowNode;
 import org.jenkinsci.plugins.workflow.steps.AbstractStepExecutionImpl;
-import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
 import org.jenkinsci.plugins.workflow.support.actions.WorkspaceActionImpl;
@@ -416,9 +415,15 @@ public class ExecutorStepExecution extends AbstractStepExecutionImpl {
                                 try {
                                     runningTasks.wait();
                                 } catch (InterruptedException x) {
-                                    // Jenkins is shutting down or this task was interrupted (Executor.doStop)
-                                    // TODO if the latter, we would like an API to StepExecution.stop the tip of our body
-                                    exec.recordCauseOfInterruption(r, listener);
+                                    // this task was interrupted (Executor.doStop)
+                                    if (Jenkins.getInstance() != null) {
+                                        // TODO we would like an API to StepExecution.stop the tip of our body
+                                        try {
+                                            exec.recordCauseOfInterruption(r, listener);
+                                        } catch (RuntimeException x2) {
+                                            LOGGER.log(Level.WARNING, null, x2);
+                                        }
+                                    } // else Jenkins is just shutting down; ignore
                                 }
                             }
                         }
