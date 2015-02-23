36f1ed3f475e8097fcff62f397658e0276e4dbbb
Jesse Glick <jglick@cloudbees.com>
2015-02-23 17:14:53 -0500

    Using SemaphoreStep is simpler and more portable than WatchYourStep.

--- cps/src/test/java/org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition2Test.java
+++ cps/src/test/java/org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition2Test.java
@@ -24,46 +24,33 @@
 
 package org.jenkinsci.plugins.workflow.cps;
 
-import hudson.FilePath;
-import java.io.File;
-import javax.inject.Inject;
-import org.jenkinsci.plugins.workflow.test.steps.WatchYourStep;
+import org.jenkinsci.plugins.workflow.test.steps.SemaphoreStep;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CpsFlowDefinition2Test extends AbstractCpsFlowTest {
 
-    @Inject WatchYourStep.DescriptorImpl watchDescriptor;
-
     /**
      * I should be able to have DSL call into async step and then bring it to the completion.
      */
     @Test public void suspendExecutionAndComeBack() throws Exception {
-        File dir = tmp.newFolder();
-
         CpsFlowDefinition flow = new CpsFlowDefinition(
-                "watch(new File('" + dir + "/marker'))\n" +
+                "semaphore 'watch'\n" +
                 "println 'Yo'");
 
         // get this going...
         createExecution(flow);
         exec.start();
 
-        // it should stop at watch and suspend.
-        exec.waitForSuspension();
-        Thread.sleep(1000);  // wait a bit to really ensure workflow has suspended
+        SemaphoreStep.waitForStart("watch/1", null);
 
         assertFalse("Expected the execution to be suspended but it has completed", exec.isComplete());
-        assertEquals(1, watchDescriptor.getActiveWatches().size());
 
         exec = roundtripXStream(exec);    // poor man's simulation of Jenkins restart
         exec.onLoad();
 
-        System.err.println("Touching the file");
-
-        // now create the marker file to resume workflow execution
-        new FilePath(new File(dir,"marker")).touch(0);
-        watchDescriptor.watchUpdate();
+        // now resume workflow execution
+        SemaphoreStep.success("watch/1", null);
 
         exec.waitForSuspension();
         assertTrue(exec.isComplete());
