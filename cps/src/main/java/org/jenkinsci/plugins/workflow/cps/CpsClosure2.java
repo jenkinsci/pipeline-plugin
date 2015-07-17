d98ecd474d18fbb1fdc48981fe0d364a18f82f2c
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-07-17 12:44:47 +0300

    print methods must be overriden to forward to CpsScript.
    Otherwise they resolve to DefaultGroovyMethods, which doesn't
    implement it correctly. The correct behavior here is to interpret it as
    a variant of the 'echo' step as CpsScript does.

--- cps/src/main/java/org/jenkinsci/plugins/workflow/cps/CpsClosure2.java
+++ cps/src/main/java/org/jenkinsci/plugins/workflow/cps/CpsClosure2.java
@@ -3,6 +3,7 @@ package org.jenkinsci.plugins.workflow.cps;
 import com.cloudbees.groovy.cps.Block;
 import com.cloudbees.groovy.cps.Env;
 import com.cloudbees.groovy.cps.impl.CpsClosure;
+import groovy.lang.Closure;
 import org.codehaus.groovy.runtime.DefaultGroovyStaticMethods;
 import org.codehaus.groovy.runtime.InvokerHelper;
 
@@ -32,5 +33,29 @@ public class CpsClosure2 extends CpsClosure {
      */
     public Object sleep(long arg) {
         return InvokerHelper.invokeMethod(getOwner(), "sleep", arg);
+    }
+
+/* Overriding methods defined in DefaultGroovyMethods
+        if we don't do this, definitions in DefaultGroovyMethods get called. One problem
+        is that most of them are not whitelisted, and the other problem is that they don't
+        always forward the call to the closure owner.
+
+        In CpsScript we override these methods and redefine them as variants of the 'echo' step,
+        so for this to work the same from closure body, we need to redefine them.
+ */
+    public void println(Object arg) {
+        InvokerHelper.invokeMethod(getOwner(), "println", new Object[]{arg});
+    }
+
+    public void println() {
+        InvokerHelper.invokeMethod(getOwner(), "println", new Object[0]);
+    }
+
+    public void print(Object arg) {
+        InvokerHelper.invokeMethod(getOwner(), "print", new Object[]{arg});
+    }
+
+    public void printf(String format, Object value) {
+        InvokerHelper.invokeMethod(getOwner(), "printf", new Object[]{format,value});
     }
 }
