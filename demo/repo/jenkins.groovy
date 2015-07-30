bcb94f879ca3b87a11a577d8440e3564f1d12b1b
Jesse Glick <jglick@cloudbees.com>
2015-07-30 14:07:07 -0400

    Defining two slaves, and refactoring script to run each parallel test on its own.

--- demo/repo/jenkins.groovy
+++ demo/repo/jenkins.groovy
@@ -1,25 +1,22 @@
 def jettyUrl = 'http://localhost:8081/'
 
-node('slave') {
+stage 'Dev'
+node {
     checkout scm
-    env.PATH="${tool 'Maven 3.x'}/bin:${env.PATH}"
-    stage 'Dev'
-    sh 'mvn -o clean package'
+    mvn '-o clean package'
     archive 'target/x.war'
+}
 
-    stage 'QA'
+stage 'QA'
+parallel(longerTests: {
+    runTests(30)
+}, quickerTests: {
+    runTests(20)
+})
 
-    parallel(longerTests: {
-        runWithServer {url ->
-            sh "mvn -o -f sometests/pom.xml test -Durl=${url} -Dduration=30"
-        }
-    }, quickerTests: {
-        runWithServer {url ->
-            sh "mvn -o -f sometests/pom.xml test -Durl=${url} -Dduration=20"
-        }
-    })
-    stage name: 'Staging', concurrency: 1
-    deploy 'target/x.war', 'staging'
+stage name: 'Staging', concurrency: 1
+node {
+    deploy 'staging'
 }
 
 input message: "Does ${jettyUrl}staging/ look good?"
@@ -28,16 +25,31 @@ try {
 } catch (NoSuchMethodError _) {
     echo 'Checkpoint feature available in CloudBees Jenkins Enterprise.'
 }
+
 stage name: 'Production', concurrency: 1
-node('master') {
+node {
     sh "wget -O - -S ${jettyUrl}staging/"
-    unarchive mapping: ['target/x.war' : 'x.war']
-    deploy 'x.war', 'production'
+    echo 'Production server looks to be alive'
+    deploy 'production'
     echo "Deployed to ${jettyUrl}production/"
 }
 
-def deploy(war, id) {
-    sh "cp ${war} /tmp/webapps/${id}.war"
+def mvn(args) {
+    sh "${tool 'Maven 3.x'}/bin/mvn ${args}"
+}
+
+def runTests(duration) {
+    node {
+        checkout scm
+        runWithServer {url ->
+            mvn "-o -f sometests/pom.xml test -Durl=${url} -Dduration=${duration}"
+        }
+    }
+}
+
+def deploy(id) {
+    unarchive mapping: ['target/x.war' : 'x.war']
+    sh "cp x.war /tmp/webapps/${id}.war"
 }
 
 def undeploy(id) {
@@ -45,9 +57,8 @@ def undeploy(id) {
 }
 
 def runWithServer(body) {
-    def jettyUrl = 'http://localhost:8081/' // TODO why is this not inherited from the top-level scope?
     def id = UUID.randomUUID().toString()
-    deploy 'target/x.war', id
+    deploy id
     try {
         body.call "${jettyUrl}${id}/"
     } finally {
