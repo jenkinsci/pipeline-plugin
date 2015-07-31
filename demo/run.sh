f848dbb88212064223ced6177429924778e1410a
Jesse Glick <jglick@cloudbees.com>
2015-07-31 13:59:49 -0400

    Better definition of jetty.sh.

--- demo/run.sh
+++ demo/run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# TODO better to define in Dockerfile
-cat << EOF > /tmp/files/jetty
-#/bin/bash
-export JETTY_ARGS=jetty.port=8081
-exec /opt/jetty/bin/jetty.sh -d supervise
-EOF
-chmod 755 /tmp/files/jetty
-/tmp/files/jetty &
+/usr/local/bin/jetty.sh &
 
 # TODO --informative-errors does not seem to be available in this version of Git
 git daemon --verbose --enable=receive-pack --base-path=/tmp/files --export-all &
