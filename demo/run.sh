0e9a7848c7abc0b70464b08e92c798ac61fc5ce4
Jesse Glick <jglick@cloudbees.com>
2015-07-31 11:00:02 -0400

    Seems to have fixed the permissions problem. But now the mock slaves are not fully working; I got java.io.IOException: Node mock-slave-2 has been deleted during the channel setup 	at hudson.slaves.SlaveComputer.setChannel(SlaveComputer.java:508) 	at hudson.slaves.SlaveComputer.setChannel(SlaveComputer.java:371) 	at org.jenkinci.plugins.mock_slave.MockSlaveLauncher.launch(MockSlaveLauncher.java:85) 	at hudson.slaves.SlaveComputer$1.call(SlaveComputer.java:243)

--- demo/run.sh
+++ demo/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-cat << EOF > /usr/bin/jetty
+# TODO better to define in Dockerfile
+cat << EOF > /tmp/files/jetty
 #/bin/bash
 export JETTY_ARGS=jetty.port=8081
 exec /opt/jetty/bin/jetty.sh -d supervise
 EOF
-chmod 755 /usr/bin/jetty
-jetty &
+chmod 755 /tmp/files/jetty
+/tmp/files/jetty &
 
 # TODO --informative-errors does not seem to be available in this version of Git
-git daemon --verbose --user=jenkins --enable=receive-pack --base-path=/tmp/files --export-all &
+git daemon --verbose --enable=receive-pack --base-path=/tmp/files --export-all &
 
-su -c '/usr/local/bin/jenkins.sh' jenkins
-# More complicated to use a clean login environment, because then we lose ENV directives from jenkinsci/docker/Dockerfile:
-#su -c 'env JENKINS_HOME=/var/jenkins_home COPY_REFERENCE_FILE_LOG=/var/log/copy_reference_file.log /usr/local/bin/jenkins.sh' - jenkins
+/usr/local/bin/jenkins.sh
