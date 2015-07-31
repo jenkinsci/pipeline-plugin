81283c06e02f128e6c5abe774e08ed0e74e5740d
Jesse Glick <jglick@cloudbees.com>
2015-07-31 12:54:08 -0400

    [JENKINS-24752] Speeding up cloud slave provisioning for this relatively quick-running demo.

--- demo/run.sh
+++ demo/run.sh
@@ -12,4 +12,8 @@ chmod 755 /tmp/files/jetty
 # TODO --informative-errors does not seem to be available in this version of Git
 git daemon --verbose --enable=receive-pack --base-path=/tmp/files --export-all &
 
+# TODO without this JENKINS-24752 workaround, it takes too long to provision.
+# (Do not add hudson.model.LoadStatistics.decay=0.1; in that case we overprovision slaves which never get used, and OnceRetentionStrategy.check disconnects them after an idle timeout.)
+export JAVA_OPTS=-Dhudson.model.LoadStatistics.clock=1000
+
 /usr/local/bin/jenkins.sh
