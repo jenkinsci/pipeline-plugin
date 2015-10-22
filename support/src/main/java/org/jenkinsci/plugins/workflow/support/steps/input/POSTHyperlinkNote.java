d44f5d440115a8418d1649c5a74292db63a8c431
Jesse Glick <jglick@cloudbees.com>
2015-10-22 17:10:39 -0400

    Turns out that moving POSTHyperlinkNote to a better package is not serialization-compatible.

--- support/src/main/java/org/jenkinsci/plugins/workflow/support/steps/input/POSTHyperlinkNote.java
+++ support/src/main/java/org/jenkinsci/plugins/workflow/support/steps/input/POSTHyperlinkNote.java
@@ -22,7 +22,7 @@
  * THE SOFTWARE.
  */
 
-package org.jenkinsci.plugins.workflow.support;
+package org.jenkinsci.plugins.workflow.support.steps.input;
 
 import hudson.Extension;
 import hudson.console.ConsoleAnnotationDescriptor;
@@ -53,7 +53,7 @@ public final class POSTHyperlinkNote extends HyperlinkNote {
 
     private final String url;
 
-    private POSTHyperlinkNote(String url, int length) {
+    public POSTHyperlinkNote(String url, int length) {
         super("#", length);
         if (url.startsWith("/")) {
             StaplerRequest req = Stapler.getCurrentRequest();
