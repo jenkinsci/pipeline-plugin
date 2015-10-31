c49749b2cb39820abc3253162ac52d0bcca80eaf
tfennelly <tom.fennelly@gmail.com>
2015-10-31 09:28:52 +0000

    Rename .js and bundle it to a different folder

--- cps/gulpfile.js
+++ cps/gulpfile.js
@@ -7,6 +7,6 @@ var builder = require('jenkins-js-builder');
 // Bundle the modules.
 // See https://github.com/tfennelly/jenkins-js-builder#bundling
 //
-builder.bundle('src/main/js/workflow.js')
+builder.bundle('src/main/js/workflow-editor.js')
     .withExternalModuleMapping('jquery-detached', 'jquery-detached:jquery2')
-    .inDir('target/generated-adjuncts/org/jenkinsci/plugins/workflow');
+    .inDir('target/generated-adjuncts/org/jenkinsci/plugins/workflow/cps');
