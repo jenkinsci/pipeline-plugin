9f1c23ff9fdb0445a5f0b407dbe7185461d3ebb7
Jesse Glick <jglick@cloudbees.com>
2015-11-16 16:49:40 -0500

    https://github.com/jenkinsci/jenkins/pull/1774 seems to have broken JenkinsRule.submit(HtmlForm, String) for YUI buttons.

--- aggregator/src/test/java/org/jenkinsci/plugins/workflow/steps/input/InputStepTest.java
+++ aggregator/src/test/java/org/jenkinsci/plugins/workflow/steps/input/InputStepTest.java
@@ -26,6 +26,8 @@ package org.jenkinsci.plugins.workflow.steps.input;
 
 import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.model.BooleanParameterDefinition;
 import hudson.model.Job;
@@ -102,7 +104,7 @@ public class InputStepTest extends Assert {
         JenkinsRule.WebClient wc = j.createWebClient();
         wc.login("alice");
         HtmlPage p = wc.getPage(b, a.getUrlName());
-        j.submit(p.getFormByName(is.getId()), "proceed");
+        submit(p.getFormByName(is.getId()), "proceed");
         assertEquals(0, a.getExecutions().size());
         q.get();
 
@@ -126,6 +128,16 @@ public class InputStepTest extends Assert {
         ApproverAction action = b.getAction(ApproverAction.class);
         assertNotNull(action);
         assertEquals("alice", action.getUserId());;
+    }
+    // TODO https://github.com/jenkinsci/jenkins/pull/1774/files#diff-a388b7a6a62410e46234342a32e29c02L1315 seems to have broken JenkinsRule.submit
+    private static HtmlPage submit(HtmlForm form, String name) throws Exception {
+        for (HtmlElement e : form.getHtmlElementsByTagName("button")) {
+            HtmlElement p = (HtmlElement) e.getParentNode().getParentNode();
+            if (p.getAttribute("name").equals(name)) {
+                return e.click();
+            }
+        }
+        throw new AssertionError("No such submit button with the name " + name);
     }
 
     @Test
