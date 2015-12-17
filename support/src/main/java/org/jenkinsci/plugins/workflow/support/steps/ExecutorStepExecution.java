f84eac1dce755b277e36834d3c329fec24a34859
Bo Stendal Sorensen <bo@stendal-sorensen.net>
2015-12-17 13:28:03 -0800

    Add EXECUTOR_NUMBER to build env

--- support/src/main/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepExecution.java
+++ support/src/main/java/org/jenkinsci/plugins/workflow/support/steps/ExecutorStepExecution.java
@@ -422,6 +422,7 @@ public class ExecutorStepExecution extends AbstractStepExecutionImpl {
                         } else {
                             env.put("NODE_NAME", label);
                         }
+                        env.put("EXECUTOR_NUMBER", String.valueOf(getExecutor().getNumber()));
 
                         synchronized (runningTasks) {
                             runningTasks.put(cookie, new RunningTask());
